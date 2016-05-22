5140d67d9af8e711473be2999239210826b79307
Nico Vinzens <Nico Vinzens>
2016-05-22 18:00:10 +0200

    calculator is now operational, courtesy of f.hauser

--- template/calculator/scripts/calculator.js
+++ template/calculator/scripts/calculator.js
@@ -1,76 +1,84 @@
 /**
  * core
  */
-var newNumber = "";
-var oldNumber = "";
-var operator = "";
-var result = "";
 
-var calculator = {
+document.calculator = {
+    newNumber: "",
+    oldNumber: "",
+    operator: "",
+    result: "",
 
     click: function (event) {
         var buttonClass = event.target.getAttribute("class");
         if (buttonClass == "number") {
-            newNumber = parseInt(event.target.getAttribute('value'));
-            ui.display(oldNumber, operator, newNumber);
+            document.calculator.newNumber = 10 * document.calculator.newNumber + parseInt(event.target.getAttribute('value'));
+            document.ui.display(document.calculator.oldNumber, document.calculator.operator, document.calculator.newNumber);
         }
         if (buttonClass == "operator") {
-            operator = event.target.getAttribute('value');
-            ui.display(oldNumber, operator, newNumber);
+            document.calculator.operator = event.target.getAttribute('value');
+            if (document.calculator.oldNumber == "") {
+                document.calculator.oldNumber = document.calculator.newNumber;
+                document.calculator.newNumber = "";
+            }
+            document.ui.display(document.calculator.oldNumber, document.calculator.operator, document.calculator.newNumber);
         }
         if (buttonClass == "command") {
             var command = event.target.getAttribute('id');
             if (command == "key-=") {
-                calculator.calculate();
+                document.calculator.calculate();
             }
             if (command == "key-c") {
-                ui.clear();
+                document.calculator.newNumber = "";
+                document.calculator.oldNumber = "";
+                document.calculator.operator = "";
+                document.calculator.result = "";
+                document.ui.clear();
             }
         }
     },
     calculate: function (event) {
-        if (operator == "+") {
-            result = oldNumber + newNumber;
+        if (document.calculator.operator == "+") {
+            document.calculator.result = document.calculator.oldNumber + document.calculator.newNumber;
         }
-        if (operator == "-") {
-            result = oldNumber - newNumber;
+        if (document.calculator.operator == "-") {
+            document.calculator.result = document.calculator.oldNumber - document.calculator.newNumber;
         }
-        if (operator == "/") {
-            result = oldNumber / newNumber;
+        if (document.calculator.operator == "/") {
+            document.calculator.result = document.calculator.oldNumber / document.calculator.newNumber;
         }
-        if (operator == "*") {
-            result = oldNumber * newNumber;
+        if (document.calculator.operator == "*") {
+            document.calculator.result = document.calculator.oldNumber * document.calculator.newNumber;
         }
-        oldNumber = "";
-        newNumber = result;
-        operator = "";
-        ui.display(oldNumber, operator, newNumber);
+        document.calculator.oldNumber = "";
+        document.calculator.newNumber = document.calculator.result;
+        document.calculator.operator = "";
+        document.ui.display(document.calculator.oldNumber, document.calculator.operator, document.calculator.newNumber);
     }
 };
 
 /**
  * UI
  */
-var ui = {
+document.ui = {
     init: function () {
-        this.input = document.querySelector(".input");
-        this.output = document.querySelector("output");
-        this.keys = document.querySelectorAll(".button");
-        this.buttons = ui.keys.parentNode.className;
-        this.buttons.on('click', calculator.click());
+        this.input = document.querySelector("#input");
+        this.output = document.querySelector("#output");
+        this.keys = document.querySelectorAll("button[type=button]");
+        this.buttons = document.ui.keys[0].parentElement;
+        this.buttons.addEventListener('click', document.calculator.click);
 
     },
     clear: function () {
-        this.input.text("");
-        this.output.text("");
+        this.input.innerHTML = "";
+        this.output.innerHTML = "";
     },
     display: function (oldNumber, operator, newNumber) {
-        this.output.text(oldNumber, operator);
-        this.input.text(newNumber);
+        this.output.innerHTML = oldNumber + " " +  operator;
+        this.input.innerHTML =  newNumber;
 
     }
 };
 
-$(function () {
-    ui.init();
+document.addEventListener('DOMContentLoaded', function() {
+    document.ui.init();
 });
